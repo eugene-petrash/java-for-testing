8d5f52baa56f37609cf6bba5772fabc10c6db4a7
Eugene Petrash <petrash.evgeniy@gmail.com>
2017-11-29 20:32:34 +0200

    Arithmetics operations and string concatenation

--- sandbox/src/main/java/petrash/custom/MyFirstProgram.java
+++ sandbox/src/main/java/petrash/custom/MyFirstProgram.java
@@ -5,6 +5,16 @@ public class MyFirstProgram {
 	public static void main(String[] args) {
 
 		System.out.println("Hello, World!");
+
+		System.out.println(2 + 2);
+
+		System.out.println(2.0 / 3);
+
+		System.out.println("2" + 2);
+
+		System.out.println("2" + "2");
+
+		System.out.println("2 + 2 = " + (2 + 2));
 	}
 
 }
