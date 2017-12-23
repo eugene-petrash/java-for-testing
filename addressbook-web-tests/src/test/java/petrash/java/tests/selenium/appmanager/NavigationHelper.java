78c7507ec58fc157b4d95f29b2b0c73fe76992a8
Eugene Petrash <petrash.evgeniy@gmail.com>
2017-12-23 20:35:15 +0200

    Go to page only if you on another page

--- addressbook-web-tests/src/test/java/petrash/java/tests/selenium/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/petrash/java/tests/selenium/appmanager/NavigationHelper.java
@@ -14,14 +14,32 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void goToContactPage() {
-        click(By.linkText("add new"));
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")
+                && isElementPresent(By.name("submit"))) {
+            return;
+        } else {
+            click(By.linkText("add new"));
+        }
+
     }
 
     public void goToGroupPage() {
-        click(By.linkText("groups"));
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        } else {
+            click(By.linkText("groups"));
+        }
     }
 
     public void returnToHomePage() {
-        click(By.linkText("home"));
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        } else {
+            click(By.linkText("home"));
+        }
+
     }
 }
