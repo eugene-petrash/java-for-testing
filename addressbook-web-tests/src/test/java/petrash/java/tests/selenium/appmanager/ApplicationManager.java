75d780d7bf4e64722111873d7220af081f1746da
Eugene Petrash <petrash.evgeniy@gmail.com>
2017-12-23 18:36:11 +0200

    Equals: compare strings correctly

--- addressbook-web-tests/src/test/java/petrash/java/tests/selenium/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/petrash/java/tests/selenium/appmanager/ApplicationManager.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -37,11 +38,11 @@ public class ApplicationManager {
     }
 
     public void init() {
-        if (browser == BrowserType.CHROME) {
+        if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
-        } else if (browser == BrowserType.FIREFOX) {
+        } else if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
-        } else if (browser == BrowserType.IE) {
+        } else if (Objects.equals(browser, BrowserType.IE)) {
             wd = new InternetExplorerDriver();
         }
 
